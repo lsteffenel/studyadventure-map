b41a2f8a8d7300376f52803545fc82cfaee40954
Luiz Angelo Steffenel <lsteffenel@gmail.com>
2025-09-17 12:18:49 +0200

    Add files via upload

---
+++ src/Horizon.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="Horizon" tilewidth="32" tileheight="32" tilecount="1980" columns="60">
+ <image source="tilesets/wp7495798-landscape-pixel-art-wallpapers.png" width="1920" height="1080"/>
+</tileset>
